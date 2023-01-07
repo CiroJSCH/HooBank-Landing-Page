3e18b8b11b8960c4531228b7ec8c179fa081ea00
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-01-07 18:33:41 -0300

    Componentes insertados en App

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
   Navbar,
   Stats,
   Testimonials,
-} from './components/index';
+} from './components';
 
 // Styles
 import styles from './styles';
@@ -19,16 +19,27 @@ const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Business Billing CardDeal Testimonials Clients CTA Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA /> 
+          <Footer />
         </div>
       </div>
     </div>
